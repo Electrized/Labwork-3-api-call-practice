7aea2259380117c4f0061b2f7371bb6af055dd94
Ely <jetu.mad@gmail.com>
2024-03-18 20:43:22 +0200

    add search bar (not functional)

--- myApp/src/pages/Tab1.tsx
+++ myApp/src/pages/Tab1.tsx
@@ -1,3 +1,4 @@
+import useApi, { SearchType } from "../apicall";
 import {
   IonApp,
   IonButton,
@@ -12,12 +13,43 @@ import {
   IonMenu,
   IonMenuToggle,
   IonPage,
+  IonSearchbar,
+  IonSelect,
+  IonSelectOption,
   IonTitle,
   IonToolbar,
   } from "@ionic/react";
+  import {useEffect, useState} from 'react';
   import "./Tab1.css";
-  import { homeOutline, reorderThreeOutline, apertureOutline } from "ionicons/icons";
+  import { homeOutline, reorderThreeOutline, apertureOutline} from "ionicons/icons";
   const Tab1: React.FC = () => {
+
+    const{ searchData } = useApi()
+    const [searchTerm, setSearchTerm] = useState('');
+    const [type, setType] = useState<SearchType>(SearchType.all);
+    const [results, setResults] = useState([]);
+
+
+    useEffect(() => {
+      console.log('SEARCH')
+      if (searchTerm === '') {
+        setResults([])
+        return
+      }
+
+      const loadData = async() => {
+        const result = await searchData(searchTerm, type)
+        setResults(result)
+        console.log("🚀 ~ loadData ~ result:", result)
+        
+        setResults(result)
+      }
+      loadData()
+
+
+
+    }, [searchTerm])
+
   return (
   <IonApp>
     <IonMenu contentId="main-content">
@@ -47,6 +79,8 @@ import {
   </IonContent>
   </IonMenu>
 
+
+
   <IonPage id="main-content">
     <IonHeader>
       <IonToolbar>
@@ -54,12 +88,44 @@ import {
           <IonMenuToggle>
         <IonButton>
           <IonIcon slot="start" icon={reorderThreeOutline}></IonIcon>
+          
         </IonButton>
       </IonMenuToggle>
     </IonButtons>
-    <IonTitle>Header</IonTitle>
+    <IonTitle>Search page</IonTitle>
+          
   </IonToolbar>
   </IonHeader>
+
+
+          <IonSearchbar 
+          value={searchTerm} 
+          debounce={300}
+          onIonChange={(e) => setSearchTerm(e.detail.value!)}
+          
+          >
+          </IonSearchbar>
+
+  <IonItem>
+    <IonLabel>Select Searchtype</IonLabel>
+    <IonSelect 
+    value={type}
+    onIonChange={(e) => setType(e.detail.value!)}
+    >
+      <IonSelectOption value="">All</IonSelectOption>
+      <IonSelectOption value="movie">Movie</IonSelectOption>
+      <IonSelectOption value="series">Series</IonSelectOption>
+      <IonSelectOption value="episode">Episode</IonSelectOption>
+
+
+    </IonSelect>
+  </IonItem>
+
+  <IonList>
+    
+  </IonList>
+
+
 
   <IonContent className="ion-padding">
     <h1>Main Content</h1>
